b76f5a6003726c150cc10eaa8c588287b6cf73a0
jairus <jairus955@gmail.com>
2018-04-25 18:41:32 +0800

    success adding change handler

--- App.js
+++ App.js
@@ -2,24 +2,43 @@ import React from 'react'
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputs: {
+        username: 'idiot username',
+        password: 'idiotic password'
+      }
+    }
+  }
+
+  handleInputChange(id, text) {
+    const { inputs } = this.state
+    const newInputs = { ...inputs, [`${id}`]: text }
+    this.setState({ inputs: newInputs })
+  }
+
   onPressLearnMore() {
 
   }
 
   render() {
+    const { inputs } = this.state
     return (
       <View style={styles.container}>
         <Text>Fucker App</Text>
         <Text>Username</Text>
         <TextInput
-          value='test'
+          value= { inputs.username }
+          onChangeText={ this.handleInputChange.bind(this, 'username') }
         />
         <Text>Password</Text>
         <TextInput
-          value='test'
+          value= { inputs.password }
+          onChangeText={ this.handleInputChange.bind(this, 'password') }
         />
         <Button
-          onPress={this.onPressLearnMore}
+          onPress={ this.onPressLearnMore }
           title='Learn More'
           color='#841584'
           accessibilityLabel='Learn more about this purple button'
